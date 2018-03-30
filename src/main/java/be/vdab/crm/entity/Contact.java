181b3878d068a07678a4959db6f50748807c46ab
patvdc <patvdc@gmail.com>
2018-03-30 14:16:28 +0200

    packages

--- src/main/java/be/vdab/crm/entity/Contact.java
+++ src/main/java/be/vdab/crm/entity/Contact.java
@@ -19,13 +19,13 @@ public class Contact {
     @Column(name="last_name")
     private String lastName;
 
-    private Blob picture;    //??facebook api
+    private Blob picture;
 
     @OneToOne
     @JoinColumn(name="lead_status")
     private LeadStatus leadStatus;
 
-    private String email;     //?? validation util using pattern
+    private String email;
 
     @OneToMany
     @JoinColumn(name = "contact_id")
