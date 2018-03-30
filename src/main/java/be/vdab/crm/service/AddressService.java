8cd53dbe5f9b44df6dfa63e345f7ceefe322053b
IvoStam <ivelinstamatov@gmail.com>
2018-03-30 15:42:27 +0200

    Added some base features for Address (service and repo)

--- src/main/java/be/vdab/crm/service/AddressService.java
+++ src/main/java/be/vdab/crm/service/AddressService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class AddressService {
@@ -15,6 +17,14 @@ public class AddressService {
 
     public void save(Address address) {
         repository.save(address);
+    }
+
+    public Address findById(int id) {
+        return repository.getOne(id);
+    }
+
+    public List<Address> getAllAddresses() {
+        return repository.findAll();
     }
 
 }
