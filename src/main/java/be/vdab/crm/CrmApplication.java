21b219312d97715331edc729336f2f8fda380aef
JohanVDN <johan.van.den.neucker@telenet.be>
2018-03-30 09:41:31 +0200

    Address Class

--- src/main/java/be/vdab/crm/CrmApplication.java
+++ src/main/java/be/vdab/crm/CrmApplication.java
@@ -5,8 +5,6 @@ import be.vdab.crm.entity.Address;
 public class CrmApplication {
 
     public static void main(String[] args) {
-        Address a = new Address();
-        a.setZipCode("12a76");
-        System.out.println(a.getZipCode());
+
     }
 }
