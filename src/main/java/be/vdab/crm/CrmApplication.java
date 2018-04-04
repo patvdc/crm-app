94fe1743638521c03b8866fbff911168ec9e0a5d
patvdc <patvdc@gmail.com>
2018-04-04 18:37:20 +0200

    thymeleaf

--- src/main/java/be/vdab/crm/CrmApplication.java
+++ src/main/java/be/vdab/crm/CrmApplication.java
@@ -3,6 +3,7 @@ package be.vdab.crm;
 import be.vdab.crm.config.CrmConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
@@ -11,7 +12,6 @@ public class CrmApplication {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext ac = SpringApplication.run(CrmApplication.class, args);
-        // SpringApplication.run(CrmConfig.class, args);
-
+        //ConfigurableApplicationContext context = new SpringApplicationBuilder(CrmConfig.class).run();
     }
 }
