42d146901ef481daae552ecb48cf980d55a1ad04
patvdc <patvdc@gmail.com>
2018-04-05 22:43:36 +0200

    userlist

--- src/main/java/be/vdab/crm/entity/User.java
+++ src/main/java/be/vdab/crm/entity/User.java
@@ -2,6 +2,7 @@ package be.vdab.crm.entity;
 
 import javax.persistence.*;
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 import java.sql.Blob;
 
@@ -11,14 +12,23 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotBlank
     private String email;
-    private String password;    //must be saved as encrypted value
+    @NotBlank
+    private String password;    //encryption !!
+
     private Integer active;
+
+    @NotBlank
     @Column(name="first_name")
     private String firstName;
+
+    @NotBlank
     @Column(name="last_name")
     private String lastName;
+
     private Language language;
+
     private Blob picture;
 
     public User() {
@@ -26,10 +36,6 @@ public class User {
 
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public String getEmail() {
