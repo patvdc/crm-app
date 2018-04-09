b2e0b228ac9e38797dd0f681c2c59b8f9606dc33
patvdc <patvdc@gmail.com>
2018-04-09 13:02:12 +0200

    authentication

--- src/main/java/be/vdab/crm/config/SecurityConfig.java
+++ src/main/java/be/vdab/crm/config/SecurityConfig.java
@@ -1,27 +1,63 @@
 package be.vdab.crm.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
-        auth.inMemoryAuthentication()
+    //    super.configure(auth);
+        auth
+                .inMemoryAuthentication()
                 .withUser("user1").password(" ").roles("SALES").and()
                 .withUser("user2").password(" ").roles("ADMIN").and()
                 .withUser("user3").password(" ").roles("SALES", "ADMIN");
+
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select email, password, true from users where email=?")
+                .authoritiesByUsernameQuery("select email, role from users where email=?");
+        //        .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // super.configure(http);
-        http.csrf().disable();
+
+        http
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/contacts").hasAnyRole("SALES")
+                .antMatchers("/quotes").hasAnyRole("SALES")
+                .antMatchers("/products").hasAnyRole("ADMIN")
+                .antMatchers("/users").hasAnyRole("ADMIN")
+                .anyRequest().permitAll()    //catch-all
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+       //         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 
     }
 }
