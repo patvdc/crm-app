248e8ab3db99801ff286e28872e41096fe284119
IvoStam <ivelinstamatov@gmail.com>
2018-04-10 14:30:16 +0200

    Fixed a regression concerning create quote being broken.

--- src/main/java/be/vdab/crm/controller/QuoteController.java
+++ src/main/java/be/vdab/crm/controller/QuoteController.java
@@ -44,30 +44,33 @@ public class QuoteController {
     public String postForm(@PathVariable(required = false) Integer contactId,
                            @ModelAttribute("quote") Quote quote, BindingResult br) {
 
-        Contact contact = contactService.findContactById(contactId);
 
+
+        Contact contact = contactService.findContactById(contactId);
         quote.setContact(contact);
         quote.setOwner(contact.getOwner()); // Temporary solution until security subsystem is more full featured...
+        refreshSalesNumbers(quote);
         quoteService.save(quote);
         System.out.println("PROCESSING FORM PSOT: " + quote + " CID: " + contactId + ", OID: " + contact.getOwner().getId() );
         return "redirect:/contacts/details/{contactId}";    // TODO
     }
 
-    @GetMapping({"/create/{id}"})
-    public String createQuoteForm(@PathVariable(required = false) Integer id, Map<String,Object> map) {
-        Contact c = contactService.findContactById(id);
-        List<Address> a = c.getAddresses();
-        Quote q = new Quote();
-        q.setContact(c);
-        quoteService.save(q);
-        System.out.println( "Quote ID:" + q.getId());
-        QuoteLine quoteLine = new QuoteLine();
-        quoteLineService.save(quoteLine);
-        List<Product> p = productService.getAllProducts();
-        map.put("products",p);
-        map.put("quote",q);
-        map.put("addresses",a);
-        map.put("quoteLine",quoteLine);
+    public void refreshSalesNumbers(Quote quote) {
+        double total = 0;
+        for (QuoteLine line : quote.getLines()) {
+            line.setProduct(productService.getProductById(line.getProduct().getId()));
+            line.setPrice(line.getProduct().getPrice());
+            line.setTotal(line.getPrice() * line.getQuantity());
+            total += line.getTotal();
+        }
+        quote.setTotal(total);
+    }
+
+    @GetMapping({"create/{contactId}"})
+    public String createQuoteForm(@PathVariable(required = false) Integer contactId, Map<String,Object> map) {
+        map.put("quote", new Quote());
+        map.put("contact", contactService.findContactById(contactId));
+        map.put("products", productService.getAllProducts());
         return "quote-create";
     }
 
