b4dac70f776ee8f86b46ab9645f892b9b8032caf
patvdc <patvdc@gmail.com>
2018-04-11 10:16:27 +0200

    activity setup

--- src/main/java/be/vdab/crm/entity/Activity.java
+++ src/main/java/be/vdab/crm/entity/Activity.java
@@ -11,11 +11,19 @@ public class Activity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Enumerated(EnumType.STRING)
     private ActivityCategory category;
-    private Date date;
-    private Time startTime;
-    private Time endTime;
-    private String comment;
-    private ActivityStatus status;
+
+    private Date date;   //date of meeting,call/todo
+    private Time startTime;    //when started
+    private Time endTime;    //when stopped
+    private String comment;     // saying that ..........
+    private String subject;    //meeting @ customer x
+
+    private ActivityStatus status;    //open  -> not yet done , closed -> done
+
+    @ManyToOne
+    private User owner;   //later
 
 }
