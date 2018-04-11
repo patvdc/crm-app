3f9b500738da09dd2df15eb3f5d4a053bf511dee
IvoStam <ivelinstamatov@gmail.com>
2018-04-11 12:41:03 +0200

    Activity list the beter one

--- src/main/java/be/vdab/crm/controller/AddressController.java
+++ src/main/java/be/vdab/crm/controller/AddressController.java
@@ -1,4 +1,28 @@
 package be.vdab.crm.controller;
 
+import be.vdab.crm.entity.Activity;
+import be.vdab.crm.service.ActivityService;
+import be.vdab.crm.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/activities")
 public class AddressController {
+    @Autowired
+    private ActivityService activityService;
+
+    @Autowired
+    private ContactService contactService;
+
+    @RequestMapping("/list")
+    public String listAllActivities(Map<String , Object> map) {
+        map.put("activityList",activityService.getAllActivities());
+        return "activity-list";
+    }
+
+
 }
